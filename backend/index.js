67c3a0637f2b060ba59b806ccacc30fa031f69cd
=Diego200013 <diegoandresrojas2000@gmail.com>
2020-05-17 22:22:53 -0500

    implement connection to mongodb

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,9 @@ const app = express();
 const moviesApi  = require('./routes/movies');
 
 //app modules
-const config = require('./config/index');
+const config = require('./config');
 
+app.use(express.json());
 moviesApi(app);
 app.listen(config.port , () => {
     console.log(`Listening http://localhost:${config.port}`);
--- backend/services/movies.js
+++ backend/services/movies.js
@@ -1,26 +1,40 @@
-const { moviesMock } = require('../utils/mocks/movies');
+const MongoLib = require('../lib/mongo');
 
 class MoviesService {
-    async getMovies() {
-        const movies = await Promise.resolve(moviesMock);
-        return movies || []
-    }
-    async getMovie() {
-        const movie = await Promise.resolve(moviesMock[0]); 
-        return movie;
-    }
-    async createMovie() {
-        const createMovieId = await Promise.resolve(moviesMock[0].id);
-        return createMovieId; 
-    }
-    async updateMovie() {
-        const updateMovieId = await Promise.resolve(moviesMock[0].id);
-        return updateMovieId; 
-    }
-    async deleteMovieId() {
-        const deleteMovieId = await Promise.resolve(moviesMock[0].id);
-        return deleteMovieId; 
-    }
+  constructor() {
+    this.collection = 'movies';
+    this.mongoDB = new MongoLib();
+  }
+
+  async getMovies({ tags }) {
+    const query = tags && { tags: { $in: tags } };
+    const movies = await this.mongoDB.getAll(this.collection, query);
+    return movies || [];
+  }
+
+  async getMovie({ movieId }) {
+    const movie = await this.mongoDB.get(this.collection, movieId);
+    return movie || {};
+  }
+
+  async createMovie({ movie }) {
+    const createMovieId = await this.mongoDB.create(this.collection, movie);
+    return createMovieId;
+  }
+
+  async updateMovie({ movieId, movie } = {}) {
+    const updatedMovieId = await this.mongoDB.update(
+      this.collection,
+      movieId,
+      movie
+    );
+    return updatedMovieId;
+  }
+
+  async deleteMovie({ movieId }) {
+    const deletedMovieId = await this.mongoDB.delete(this.collection, movieId);
+    return deletedMovieId;
+  }
 }
 
 module.exports = MoviesService;
