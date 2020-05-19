3ebe96a0a6623ed9b831e2b7e771ad16eb06a1bc
=Diego200013 <diegoandresrojas2000@gmail.com>
2020-05-18 22:11:30 -0500

    handler errors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const moviesApi  = require('./routes/movies');
-
+const { logErrors  , errorHandler} = require('./utils/middlewares/errorHandlers');
 //app modules
 const {config} = require('./config');
 
 app.use(express.json());
 moviesApi(app);
+
+// middlewares de error
+app.use(logErrors)
+app.use(errorHandler)
+
 app.listen(config.port , () => {
     console.log(`Listening http://localhost:${config.port}`);
 })
