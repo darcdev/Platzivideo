c0868a31577e9d37d1dacf451ec93c92ed753447
=Diego200013 <diegoandresrojas2000@gmail.com>
2020-05-18 22:34:23 -0500

    handling errors with boom

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
 const moviesApi  = require('./routes/movies');
-const { logErrors  , errorHandler} = require('./utils/middlewares/errorHandlers');
+const { logErrors  ,wrapErrors ,  errorHandler} = require('./utils/middlewares/errorHandlers');
+const notFoundHandler = require('./utils/middlewares/notFoundHandler');
+
 //app modules
 const {config} = require('./config');
 
 app.use(express.json());
 moviesApi(app);
-
+app.use(notFoundHandler)
 // middlewares de error
 app.use(logErrors)
+app.use(wrapErrors)
 app.use(errorHandler)
 
 app.listen(config.port , () => {
--- backend/utils/middlewares/errorHandlers.js
+++ backend/utils/middlewares/errorHandlers.js
@@ -1,21 +1,30 @@
 const { config } = require('../../config');
+const boom = require('boom');
 
 function withErrorStack(error, stack){
     if(config.dev){
-        return  { error , stack}
+        return  { ...error , stack}
     }
     return error;
 }
-
+function wrapErrors(err,req,res,next){
+    if(!err.isBoom) next(boom.badImplementation(err));
+    next(err);
+}
 function logErrors(err,req,res,next){
     console.log(err);
     next(err);
 }
 function errorHandler(err,req,res,next){ // eslint-disable-line
-    res.status(err.status || 500);
-    res.json(withErrorStack(err.message, err.stack))
+    const { 
+        output : {payload ,statusCode}
+    } = err;
+
+    res.status(statusCode);
+    res.json(withErrorStack(payload, err.stack))
 }
 module.exports = {
     logErrors,
-    errorHandler
+    errorHandler,
+    wrapErrors
 }
