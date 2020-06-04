a269388d25263dc72bc4b8d4dfe4f6e573c3f2a1
=Diego200013 <diegoandresrojas2000@gmail.com>
2020-06-04 14:48:44 -0500

    header structure

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+var App = function App() {
+  return (
+    <header className="header">
+      <img
+        className="header__img"
+        src="../assets/logo-platzi-video-BW2.png"
+        alt="Platzi Video"
+      />
+      <div className="header__menu">
+        <div className="header__menu--profile">
+          <img src="./assets/user-icon.png" alt="" />
+          <p>Perfil</p>
+        </div>
+        <ul>
+          <li>
+            <a href="/">Cuenta</a>
+          </li>
+          <li>
+            <a href="/">Cerrar Sesión</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default App;
---
+++ frontend/src/containers/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
+
+var App = function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
