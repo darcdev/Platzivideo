65ccff976095d32320664accb8238b95ed4e596e
darcdev <diegoandresrojas2000@gmail.com>
2020-06-26 16:22:43 -0500

    add register container

---
+++ frontend/src/containers/notFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = function NotFound() {
+  return (
+    <section className="error">
+      <p className="error__not-found-error">404</p>
+      <p className="error__not-found-title">Pagina No encontrada</p>
+    </section>
+  );
+};
+
+export default NotFound;
