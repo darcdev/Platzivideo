0eb298a35b52c21fc237538fc1dbb398f911f924
darcdev <diegoandresrojas2000@gmail.com>
2020-06-27 21:13:01 -0500

    nodemon & dotenv

--- frontend/src/server/server.js
+++ frontend/src/server/server.js
@@ -1,12 +1,19 @@
-const express = require('express');
+import express from 'express';
+import config from './config';
 
+const { env, port } = config;
 const app = express();
 
+if (env === 'development') {
+  console.log('development config');
+}
 app.get('*', (req, res) => {
+  console.log('recargando');
   res.send({ app: 'server' });
 });
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
   if (err) console.log(err);
   else console.log('escuchando puerto 3000');
+  console.log(port);
 });
