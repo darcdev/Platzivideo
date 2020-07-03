41a7e170707e5ec56173ba781029045951636b9b
darcdev <diegoandresrojas2000@gmail.com>
2020-07-02 21:51:22 -0500

    jwt strategy

---
+++ backend/utils/auth/strategies/jwt.js
@@ -0,0 +1,25 @@
+const passport = require('passport');
+const { Strategy, ExtractJwt } = require('passport-jwt');
+const boom = require('@hapi/boom');
+
+const { config } = require('../../../config/index');
+const UsersService = require('../../../services/users');
+
+passport.use(
+  new Strategy({
+    secretOrKey: config.authJwtSecret,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  }),
+  async function (tokenPayload, cb) {
+    const userService = new UsersService();
+    try {
+      const user = userService.getUser({ email: tokenPayload.email });
+      if (!user) {
+        return cb(boom.unauthorized(), false);
+      }
+      return cb(null, { ...user, scopes: tokenPayload.scopes });
+    } catch (error) {
+      return cb(error);
+    }
+  }
+);
